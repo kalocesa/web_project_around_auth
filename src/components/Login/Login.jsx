46f0e8169d9b9ee06890ddaff7a0afb5057c88df
kalocesa <kalocesa95@gmail.com>
2025-05-06 00:32:12 -0600

    add: estilos y archivo jsx a componente login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
 export const Login = () => {
-  return <p>Holiiii el login</p>;
+  return (
+    <div className="login_container">
+      <h1 className="login_title">Inicia sesión</h1>
+      <form>
+        <fieldset>
+          <input type="email" placeholder="Correo electrónico" />
+          <input type="password" placeholder="Contraseña" />
+        </fieldset>
+        <button className="login_button">Inicia sesión</button>
+      </form>
+      <p className="login_text">
+        ¿Aún no eres miembro? Regístrate{" "}
+        <Link to={"/signin"} className="login_link">
+          aquí
+        </Link>
+      </p>
+    </div>
+  );
 };
