2af5fd2a4e49e4ca4e72de18d543c4fcd60e5e9e
kalocesa <kalocesa95@gmail.com>
2025-05-07 20:13:54 -0600

    fix: register.jsx

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,23 +1,38 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Intentando registrar usuario con:", email, password);
+  };
+
   return (
     <div className="register_container">
       <h1 className="register_title">Regístrate</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset className="register_fieldset">
           <input
             className="register_input"
             type="email"
             placeholder="Correo electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="register_input"
             type="password"
             placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </fieldset>
-        <button className="register_button">Regístrate</button>
+        <button className="register_button" type="submit">
+          Regístrate
+        </button>
       </form>
       <p className="register_text">
         ¿Ya eres miembro? Inicia sesión{" "}
